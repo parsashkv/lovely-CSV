1dd2a9d1867d53352e2aa709bbe001ca2e481e64
Parsa Shirkavand <parsa.shirkavand1383@gmail.com>
2026-06-05 12:34:56 +0330

    made another file which includes total price of each product.

---
+++ CSV.py
@@ -0,0 +1,10 @@
+import pandas as pd
+df = pd.read_excel("product.csv")
+
+df["total_price"] = df["price"] * df["quantity"]
+total_sum = df["total_price"].sum()
+print(df)
+print("Total Sum =", total_sum)
+
+df.to_csv("result.csv", index=False)
+
